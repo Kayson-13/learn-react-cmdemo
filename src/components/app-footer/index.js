8190bae40c3aa95397d8cb9d6adadb6c71e5b04b
test <543358961@qq.com>
2020-09-15 17:27:39 +0800

    footer day01

--- src/components/app-footer/index.js
+++ src/components/app-footer/index.js
@@ -1,9 +1,14 @@
 import React, { memo } from 'react'
 
+import { FooterWrapper, FooterLeft, FooterRight } from './style'
+
 export default memo(function CMAppFooter() {
   return (
-    <div>
-      <h2>AppFooter</h2>
-    </div>
+    <FooterWrapper>
+      <div className="content wrap-v2">
+        <FooterLeft>footerLeft</FooterLeft>
+        <FooterRight>footerRight</FooterRight>
+      </div>
+    </FooterWrapper>
   )
 })
---
+++ src/components/app-footer/style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const FooterWrapper = styled.div`
+  background-color: #f2f2f2;
+  color: #666;
+  height: 172px;
+  border-top: 1px solid #d3d3d3;
+  
+  .content {
+    display: flex;
+    justify-content: space-between;
+    font-size: 12px;
+  }
+`
+
+export const FooterLeft = styled.div`
+
+`
+
+export const FooterRight = styled.div`
+
+`
