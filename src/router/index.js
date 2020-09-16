82b46a80262d958e2296e9f1630f0666102a7a89
test <543358961@qq.com>
2020-09-16 14:46:53 +0800

    update directory2

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,18 @@
+import React from 'react';
+
 import CMDiscovery from '@/pages/discovery';
 import CMFriends from '@/pages/friends';
 import CMProfile from '@/pages/profile';
+import { Redirect } from 'react-router-dom';
 
 const routes = [
   {
+    path: "/",
+    exact: true,
+    render: () => (
+      <Redirect to="/discovery"/>
+    )
+  },{
     path: "/discovery",
     component: CMDiscovery
   },
